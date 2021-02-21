16f4e4102306e327ac8e3a66df34422f6f249888
Trevor Roberts Jr <trevorr@amazon.com>
2021-02-21 15:04:38 -0500

    added capability to interpret query parameter 'test'

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	// fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	// u, err := url.Parse(s)
+	// key: test
+	// value: wafrule
+	keys, ok := r.URL.Query()["test"]
+
+	if !ok || len(keys[0]) < 1 {
+		log.Println("Url Param 'test' is missing if you want to evaluate query strings")
+		fmt.Fprintf(w, "Hi there, I love %s!\n", r.URL.Path[1:])
+		return
+	}
+	fmt.Fprintf(w, "Hi there, I love %s!\n", keys[0])
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
